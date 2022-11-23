0b33552e051e1ddbf69afacef3ca09db28c1076d
truongmn203 <106716785+truongmn203@users.noreply.github.com>
2022-11-23 21:10:52 +0700

    mactruong moi

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,5 +1,29 @@
-const $$ = document.querySelectorAll.bind(document);
-// const lala = $(".lala");
-var api = "http://localhost:3000/games";
+// validete
+function validete() {
+    const loiEmail = document.getElementById("loigmail");
+    const loipassword = document.getElementById("loipassword");
+    var gmail = document.frn.tgmail.value;
+    var tpassword = document.frn.password.value;
+    var ok = true;
 
+    //kiem tra gmail
+    var regmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (gmail == "") {
+        loiEmail.innerHTML = "Email là bắt buộc";
+        ok = false;
+    } else if (!gmail.match(regmail)) {
+        loiEmail.innerHTML = "Email không hợp lệ.";
+        ok = false;
+    } else loiEmail.innerHTML = "";
+    //kiem tra Password
+    if ( tpassword  == "") {
+        loipassword.innerHTML = "Password là bắt buộc";
+        ok = false;
+    } else loipassword.innerHTML = "";
 
+    //tong ket kiem tra
+    if (ok) {
+        // alert("dang ki thanh cong");
+        return true;
+    } else return false;
+}
