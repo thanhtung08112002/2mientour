9b24beb140ba50109c991a1e48f336d06c30977c
Tran Tien Dung <trantiendung260920033@gmail.com>
2022-12-02 12:22:15 +0700

    bye

--- app/views/admin/login_success/add_account/add_account.php
+++ app/views/admin/login_success/add_account/add_account.php
@@ -1,29 +1,40 @@
 <article>
-    <div class="headline">
-        <h2>Thêm tài khoản</h2>
-    </div>
-    <form action="save_account" method="post" enctype="multipart/form-data">
-        <div class="row">
-            <div class="col">
-                <div class="form-group">
-                    <label for="">Mã account</label>
-                    <input class="form-control" type="text" name="id_account" readonly placeholder="Auto number" disabled>
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <label for="">Tài Khoản</label>
-                    <input class="form-control" type="text" name="tai_khoan"  >
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <label for="">Mật khẩu</label>
-                    <input class="form-control" type="text" name="mat_khau"  >
-                </div>
-            </div>
+    <div class="horizontal-menu">
+        <div class="user">
+            <p class="user-name">Mot Hai Ba</p>
+            <img src="images.jfif" alt="">
         </div>
-        <button class="btn" type="submit" name="btn_insert">Thêm</button>
-        <button class="btn"><a href="list_accounts">Danh sách</a></button>
-    </form>
+    </div>
+    <!-- bảng -->
+    <div class="board">
+        <div class="headline">
+            <h2>Thêm tài khoản</h2>
+        </div>
+        <form action="save_account" method="post" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Mã account</label>
+                        <input class="form-control" type="text" name="id_account" readonly placeholder="Auto number" disabled>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Tài Khoản</label>
+                        <input class="form-control" type="text" name="tai_khoan">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="">Mật khẩu</label>
+                        <input class="form-control" type="text" name="mat_khau">
+                    </div>
+                </div>
+            </div>
+            <button class="btn" type="submit" name="btn_insert">Thêm</button>
+            <button class="btn"><a href="list_accounts">Danh sách</a></button>
+        </form>
+    </div>
+
+    <!-- end bảng -->
 </article>
--- app/views/admin/login_success/header_admin.php
+++ app/views/admin/login_success/header_admin.php
@@ -5,13 +5,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/admin/admin.css" />
-    <link rel="stylesheet" href="public/css/admin/pageAdmin.css"/>
-    <link rel="stylesheet" href="public/css/admin/form_login.css"/>
+    <link rel="stylesheet" href="public/css/admin/pageAdmin.css" />
+    <link rel="stylesheet" href="public/css/admin/form_login.css" />
+    <link rel="stylesheet" href="public/css/admin/header_admin.css" />
+
 
     <title>Document</title>
 </head>
 
 <body>
     <div class="container">
+        <header>
+            <div class="menu ">
+                <div class="menu-logo">
+                    <img src="logo_header.jpg" alt="">
+                    <h2>Nhóm 7</h2>
+                </div>
+                <nav class="menu_nav">
+                    <ul>
+                        <li><a href="#">Trang chủ</a></li>
+                        <li><a href="#">Danh sách sản phẩm</a></li>
+                        <li><a href="#">Khách hàng</a></li>
+                        <li><a href="#">Loại Hàng</a></li>
+                        <li><a href="#">Bình luận</a></li>
+                        <li><a href="#">Thống kê</a></li>
+                        <li><a href="#">Sign out</a></li>
+                    </ul>
+                </nav>
+            </div>
 
+        </header>
